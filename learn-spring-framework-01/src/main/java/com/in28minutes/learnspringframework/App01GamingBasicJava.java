1fb7d55c13becdd58e625151d7533981da228743
DooHanr <dhe_crysis@naver.com>
2023-11-21 18:19:38 +0900

    this is commit test

--- learn-spring-framework-01/src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
+++ learn-spring-framework-01/src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
@@ -9,6 +9,7 @@ public class App01GamingBasicJava {
 		
 		//var game = new MarioGame();
 		//var game = new SuperContraGame();
+		//this is text
 		
 		var game = new PacmanGame(); //1: Object Creation
 		
