f334db61f35f7f94d8fff9ba7c9c507cfb7f4180
DooHanR <105795149+DooHanR@users.noreply.github.com>
2023-11-22 21:14:11 +0900

    after study.
    직접 실제 Spring Framework 를 통해 Configuration 과 ComponentScan 등을 통해 Component 등을 가져하고 injection에 대해 실습해봣는데 보통 생성자 인젝션을 쓴다고는 하는데 보면 인터페이스를 인젝션 해서 그 아래의 것들도 함께 가져오는듯한 아이고 머리에 정리가 하나도 안되는 어렵고 복잡한 그리고 갑자기 게으른 초기화와 즉시 초기화에 대해 배웠고 게으른 초기화를 하면 안된다 그러고 보니까 초기화가 실행직전에되서 그럴거 같기도하고 어렵다

---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/c1/DataService.java
@@ -0,0 +1,14 @@
+package com.in28minutes.learnspringframework.examples.c1;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+
+public interface DataService {
+	int[] retrieveData();
+}
