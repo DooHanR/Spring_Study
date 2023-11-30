0e2055335957f440f4f02184ecd309e1d038343b
DooHanR <105795149+DooHanR@users.noreply.github.com>
2023-11-30 14:48:15 +0900

    아오 오늘 뭐배웠냐
    그 Spring JDBC에서 메인 클래스 이전의 다른 잡 클래스에서 INSERT_QUERY, DELETE_QUERY, SELECT_QUERY등을 직접 string으로 타이핑 한다음에 그걸 호출하는 함수등을 직접 작성했다. INSERT나 DELETE는 똑같았는데 SELECT_QUERY는 함수의 반환자로 Object를 받더라고. Course가 아니라. 하여튼간에 그렇게 받고 두번째 매개변수로도 별 해괴한게 아니라 매핑용도로 가져오는데 id랑 특정해서 그렇게 했더니 짜잔 콘솔에 출력이 됩니다. 하여튼 Spring JDBC를 써봤는데 자바 코드는 간단해도 역시나 쿼리코드가 적잖게 있어서 이것보다 나은 JPA를 써보겠다고. 그래서 JPA에서 나오는 개념이 몇개좀 있다. 바로 Entitiy와 Manager 라는건데. 매핑할 클래스가 Entitiy 어노테이션이 추가되고 그 밑에 프로퍼티 들에도 @Id, @Column 식으로 추가되더라. 이때 클래스 이름이 다르면 (...) 식으로 추가적으로 들어가는데 여기선 같아서 그런짓거리는 안했고 그다음에 본격적으로 엔티티 매니저 공부좀 하려다가 여기서 끝남.

--- learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.in28minutes.springboot.learnspringboot.Course;
+
 @Component
 public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 	
@@ -13,7 +15,12 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		repository.insert();
+		repository.insert(new Course(1, "Learn AWS now!", "in28minutes"));
+		repository.insert(new Course(2, "Test", "in28minutes"));
+		
+		repository.deleteById(1);
+	
+		
+		System.out.println(repository.findById(2));
 	}
-
 }
